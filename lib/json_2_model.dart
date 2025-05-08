52796f9062630eff65da7b806abd0e297315210a
来彬 <568334413@qq.com>
2025-05-08 14:50:36 +0800

    init

---
+++ lib/json_2_model.dart
@@ -0,0 +1,7 @@
+library json_2_model;
+
+/// A Calculator.
+class Json2Model {
+  const Json2Model();
+}
+const json2Model = Json2Model();
---
+++ test/json_2_model_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:json_2_model/json_2_model.dart';
+
+void main() {
+  test('adds one to input values', () {
+    // expect(calculator.addOne(2), 3);
+    // expect(calculator.addOne(-7), -6);
+    // expect(calculator.addOne(0), 1);
+  });
+}
